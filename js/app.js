54f60bb114a655b1028d129ce8e2a0d107c5edfb
jessica_b <jessica.francavilla86@gmail.com>
2021-11-17 22:29:05 -0300

    add css styles

--- js/app.js
+++ js/app.js
@@ -45,6 +45,24 @@ var app = new Vue({
         portada:
           "https://image.shutterstock.com/image-photo/antique-tea-cup-full-on-260nw-473004682.jpg",
       },
+      {
+        nombre: "Latte 2",
+        precio: "300",
+        portada:
+          "https://previews.123rf.com/images/amenic181/amenic1811702/amenic181170200033/72395893-cup-of-coffee-latte-and-coffee-beans-isolated-on-white-background.jpg",
+      },
+      {
+        nombre: "Capuccino 2",
+        precio: "400",
+        portada:
+          "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-CAPPUCCINO-BANANA-SESAME-SEEDS.jpg",
+      },
+      {
+        nombre: "Tea 2",
+        precio: "500",
+        portada:
+          "https://image.shutterstock.com/image-photo/antique-tea-cup-full-on-260nw-473004682.jpg",
+      },
     ],
   },
 });
