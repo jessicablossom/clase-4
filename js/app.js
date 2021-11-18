c72cb42e81a857f0fa921ff40648ef5078614c2b
jessica_b <jessica.francavilla86@gmail.com>
2021-11-17 22:19:45 -0300

    add catalog of items

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 Vue.component("navbar", {
   template: `<nav>
-    <div>
       <ul>
         <li><a href>Link</a></li>
         <li><a href>Link</a></li>
       </ul>
-    </div>
   </nav>`,
 });
 
@@ -14,7 +12,39 @@ Vue.component("component", {
   <p>Body foam brewed, café au lait macchiato crema dripper sugar. And grinder lungo cinnamon french press affogato dark blue mountain instant. Acerbic macchiato skinny cup single shot mug trifecta. Crema ut, roast brewed affogato spoon single origin. Plunger pot seasonal flavour, medium, black cultivar americano whipped that a robusta.</p>`,
 });
 
+Vue.component("card", {
+  props: ["comida"],
+  template: `
+    <div class="card">
+    <p> {{comida.nombre}}</p>
+    <p> $ {{comida.precio}}</p>
+    <img :src="comida.portada"/>
+    </div>
+    `,
+});
+
 var app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    comidas: [
+      {
+        nombre: "Latte",
+        precio: "300",
+        portada:
+          "https://previews.123rf.com/images/amenic181/amenic1811702/amenic181170200033/72395893-cup-of-coffee-latte-and-coffee-beans-isolated-on-white-background.jpg",
+      },
+      {
+        nombre: "Capuccino",
+        precio: "400",
+        portada:
+          "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-CAPPUCCINO-BANANA-SESAME-SEEDS.jpg",
+      },
+      {
+        nombre: "Tea",
+        precio: "500",
+        portada:
+          "https://image.shutterstock.com/image-photo/antique-tea-cup-full-on-260nw-473004682.jpg",
+      },
+    ],
+  },
 });
